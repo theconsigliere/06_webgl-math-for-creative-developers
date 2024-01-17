10e5a1ee9c3357a91de745a1fb760d2f6df9f581
Dirty Martini <hello@maxwellkirwin.co.uk>
2024-01-17 21:43:51 +0000

    done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import { createNoise2D } from "simplex-noise"
+let noise = createNoise2D()
 
 const width = window.innerWidth,
   height = window.innerHeight
@@ -9,7 +11,6 @@ const width = window.innerWidth,
 const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10)
 camera.position.z = 1
 camera.position.y = 1
-camera.position.x = 1
 // look at center of scene
 camera.lookAt(0, 0, 0)
 
@@ -21,45 +22,28 @@ const divisions = 40
 const gridHelper = new THREE.GridHelper(size, divisions)
 scene.add(gridHelper)
 
-// orbit controls
-
-let mesh = new THREE.Mesh(
-  new THREE.SphereGeometry(0.05, 32, 32),
+let ball = new THREE.Mesh(
+  new THREE.SphereGeometry(0.025, 32, 32),
   new THREE.MeshNormalMaterial()
 )
 
-function addPoint(x, y, z) {
-  let point = mesh.clone()
-  point.position.set(x, y, z)
-  scene.add(point)
+let balls = []
 
-  return point
-}
+for (let i = 0; i < 50; i++) {
+  let mesh = ball.clone()
 
-let number = 300
-
-let objects = []
-
-// makes a full circle of spheres
-for (let i = 0; i < number; i++) {
-  // math PI * 2 = 360 degrees (full circle)
-  let theta = (i / number) * Math.PI * 2
-  let x = Math.cos(theta)
-  let y = Math.sin(theta)
-  let z = 0
-
-  let mesh = addPoint(x, y, z)
-  objects.push({
+  balls.push({
     mesh,
-    theta,
-    random: Math.random(),
-    x: Math.random() / 5,
-    y: Math.random() / 5,
-    z: Math.random() / 5,
+    offset: Math.random(),
+    index: i,
   })
+
+  scene.add(mesh)
 }
 
+// orbit controls
 const renderer = new THREE.WebGLRenderer({ antialias: true })
+
 let controls = new OrbitControls(camera, renderer.domElement)
 renderer.setSize(width, height)
 renderer.setAnimationLoop(animation)
@@ -68,16 +52,16 @@ document.body.appendChild(renderer.domElement)
 // animation
 
 function animation(time) {
-  objects.forEach((object, i) => {
-    let { mesh, theta, random, x, y, z } = object
-    let newx = Math.cos(theta + time / 1000) + x
-    let newy = Math.sin(theta + time / 1000) + y
-    let newz = z
+  let progress = (time / 2000) % 1
 
-    mesh.position.set(newx, newy, newz)
+  //ball.position.y = noise(time / 1000, 0) * power
+
+  balls.forEach((ball, i) => {
+    let uniqueProgress = (progress + ball.offset) % 1 // progress will always be between 0 and 1
+    ball.mesh.position.x = uniqueProgress - 0.5
+    // osicllation
+    let power = Math.sin(uniqueProgress * Math.PI)
+    ball.mesh.position.y = noise(time / 1000, ball.index) * power
   })
-  mesh.rotation.x = time / 2000
-  mesh.rotation.y = time / 1000
-
   renderer.render(scene, camera)
 }
